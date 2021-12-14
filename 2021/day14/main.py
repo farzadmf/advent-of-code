0bb529a3093966979fcbd42d13fe678fe2c890a8
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2021-12-14 13:43:36 -0500

    Change to use maps to be able to do day 14 part 2

--- 2021/day14/main.py
+++ 2021/day14/main.py
@@ -1,5 +1,6 @@
 from collections import (
     deque,
+    defaultdict,
     Counter,
 )
 import sys
@@ -37,22 +38,37 @@ def read_input(input_file: str) -> Tuple[Template, Pairs, Rules]:
     return template, pairs, rules
 
 
-def main(input_file: str) -> None:
+def main(input_file: str, steps: int) -> None:
     template, pairs, rules = read_input(input_file)
     counter = Counter(template)
 
-    q = deque(pairs)
-    for _ in range(10):
-        for _ in range(len(q)):
-            p = q.popleft()
-            ps, added = process(p, rules)
-            for p in ps:
-                q.append(p)
-            counter[added] += 1
+    # Help from here: https://git.io/JDEXZ
+    # idea to not manipulate existing 'pair_counts' and create
+    #   a new 'new_counts' and assign it after each loop
+    cache: Dict[Template, Tuple[List[Template], str]] = {}
+    pair_counts: Dict[str, int] = defaultdict(int)
+    new_counts: Dict[str, int] = defaultdict(int)
+
+    for p in pairs:
+        cache[p] = process(p, rules)
+        pair_counts[p] += 1
+
+    for _ in range(steps):
+        new_counts = defaultdict(int)
+        for pair, count in pair_counts.items():
+            if count <= 0:
+                continue
+
+            pairs, added = process(pair, rules)
+            counter[added] += count
+            for p in pairs:
+                new_counts[p] += count
+
+        pair_counts = new_counts
 
     values = sorted(counter.values())
     print(values[-1] - values[0])
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(sys.argv[1], int(sys.argv[2]))
