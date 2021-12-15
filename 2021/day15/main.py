c8df321b16b65d46361bb10c9a5819d8fc4ec23e
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2021-12-15 01:53:41 -0500

    Add [BROKEN] day 15 part 2

--- 2021/day15/main.py
+++ 2021/day15/main.py
@@ -9,37 +9,59 @@ def read_input(input_file: str) -> List[str]:
         return f.read().splitlines()
 
 
+def repeat(row: List[int]) -> List[int]:
+    res = []
+    for r in row:
+        res.append(1 if r == 9 else r + 1)
+    return res
+
+
 class Grid:
     def __init__(self, lines: List[str]):
         self.rows = len(lines)
         self.cols = len(lines[0])
         self.cells = [[0] * self.cols for _ in range(self.rows)]
-        self.start = (0, 0)
-        self.end = (self.rows - 1, self.cols - 1)
+        self.dp = []
+
         for r, l in enumerate(lines):
             for c, char in enumerate(l):
                 self.cells[r][c] = int(char)
 
+    def expand(self):
+        for r in self.cells:
+            rep = r
+            for _ in range(4):
+                res = repeat(rep)
+                r += res
+                rep = res
+        self.cols *= 5
+        for i in range(4):
+            for r in range(self.rows):
+                row = self.cells[r + (self.rows * i)]
+                self.cells.append(repeat(row))
+        self.rows *= 5
+
     def find_lowest_risk(self) -> int:
-        dp = self.cells.copy()
-        dp[0][0] = 0
+        self.dp = [row[:] for row in self.cells]
+        self.dp[0][0] = 0
 
         for r in range(1, self.rows):
-            dp[r][0] += dp[r - 1][0]
+            self.dp[r][0] += self.dp[r - 1][0]
         for c in range(1, self.cols):
-            dp[0][c] += dp[0][c - 1]
+            self.dp[0][c] += self.dp[0][c - 1]
 
         for r in range(1, self.rows):
             for c in range(1, self.cols):
-                up, left = dp[r - 1][c], dp[r][c - 1]
-                dp[r][c] += min(up, left)
+                up, left = self.dp[r - 1][c], self.dp[r][c - 1]
+                self.dp[r][c] += min(up, left)
 
-        return dp[-1][-1]
+        return self.dp[-1][-1]
 
 
 def main(input_file: str) -> None:
     lines = read_input(input_file)
     grid = Grid(lines)
+    grid.expand()
     print(grid.find_lowest_risk())
 
 
