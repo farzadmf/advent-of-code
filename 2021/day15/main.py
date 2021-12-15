4aa3f472f60dc26c996008dfb94a6baadf814313
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2021-12-15 10:14:16 -0500

    Use Dijkstra for part 2 (and part 1)

--- 2021/day15/main.py
+++ 2021/day15/main.py
@@ -1,3 +1,7 @@
+from heapq import (
+    heappush,
+    heappop,
+)
 import sys
 from typing import (
     List,
@@ -21,7 +25,7 @@ class Grid:
         self.rows = len(lines)
         self.cols = len(lines[0])
         self.cells = [[0] * self.cols for _ in range(self.rows)]
-        self.dp = []
+        self.end = (self.rows - 1, self.cols - 1)
 
         for r, l in enumerate(lines):
             for c, char in enumerate(l):
@@ -40,29 +44,31 @@ class Grid:
                 row = self.cells[r + (self.rows * i)]
                 self.cells.append(repeat(row))
         self.rows *= 5
+        self.end = (self.rows - 1, self.cols - 1)
 
-    def find_lowest_risk(self) -> int:
-        self.dp = [row[:] for row in self.cells]
-        self.dp[0][0] = 0
-
-        for r in range(1, self.rows):
-            self.dp[r][0] += self.dp[r - 1][0]
-        for c in range(1, self.cols):
-            self.dp[0][c] += self.dp[0][c - 1]
-
-        for r in range(1, self.rows):
-            for c in range(1, self.cols):
-                up, left = self.dp[r - 1][c], self.dp[r][c - 1]
-                self.dp[r][c] += min(up, left)
-
-        return self.dp[-1][-1]
+    def find_lowest_risk_bfs(self) -> int:
+        q = [(0, (0, 0))]
+        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while q:
+            cost, (r, c) = heappop(q)
+            if self.cells[r][c] == -1:
+                continue
+            self.cells[r][c] = -1
+            if (r, c) == self.end:
+                return cost
+            for dr, dc in dirs:
+                nr, nc = r + dr, c + dc
+                if not (0 <= nr < self.rows and 0 <= nc < self.cols):
+                    continue
+                heappush(q, (cost + self.cells[nr][nc], (nr, nc)))
+        return -1
 
 
 def main(input_file: str) -> None:
     lines = read_input(input_file)
     grid = Grid(lines)
     grid.expand()
-    print(grid.find_lowest_risk())
+    print(grid.find_lowest_risk_bfs())
 
 
 if __name__ == "__main__":
