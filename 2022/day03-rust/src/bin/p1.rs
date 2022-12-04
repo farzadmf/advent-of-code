528ec85def1ce5ce68918514fa1cc8a07ad5815f
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2022-12-03 22:56:42 -0500

    Replace with what I saw on Reddit (flat_map seems to call unwrap on the whole thing in this case)

--- 2022/day03-rust/src/bin/p1.rs
+++ 2022/day03-rust/src/bin/p1.rs
@@ -1,22 +1,17 @@
-use std::collections::HashSet;
-
 use day03_rust::{get_code, read_input};
 
 fn main() {
     println!("+++++++++++++++++++ PART 1 +++++++++++++++++++");
 
     let input = read_input("input".to_string());
-    let result: u32 = input
+
+    // From here: https://bit.ly/3F3fv3R
+    let result = input
         .lines()
         .map(|line| line.split_at(line.len() / 2))
-        .map(|(first, second)| {
-            let first_chars: HashSet<char> = first.chars().collect();
-            let second_chars: HashSet<char> = second.chars().collect();
-
-            let shared = first_chars.intersection(&second_chars).nth(0).unwrap();
-            get_code(*shared).unwrap()
-        })
-        .sum();
+        .flat_map(|(first, second)| first.chars().find(|&c| second.contains(c)))
+        .flat_map(get_code)
+        .sum::<u32>();
 
     println!("result: {}", result);
 
