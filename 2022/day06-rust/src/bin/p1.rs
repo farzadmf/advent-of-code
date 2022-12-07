cb762074475a564c33d9adb262e9b44ed972c6dd
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2022-12-06 22:56:27 -0500

    2022-D6: part 1

--- 2022/day06-rust/src/bin/p1.rs
+++ 2022/day06-rust/src/bin/p1.rs
@@ -1,9 +1,14 @@
+use day06_rust::{get_packet_start, read_input};
+
 fn main() {
     println!("+++++++++++++++++++ PART 1 +++++++++++++++++++");
-    let result = 0;
 
-    // Let's do this!
+    let input = read_input("input");
 
-    println!("result: {}", result);
+    input.lines().for_each(|line| {
+        let start = get_packet_start(line);
+        println!("got {}", start);
+    });
+
     println!("------------------- PART 1 -------------------");
 }
