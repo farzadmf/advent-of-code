0dc8031daca75ed4dc15a6b52d9e2d3c22220fad
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2022-12-06 23:17:41 -0500

    2022-D6: learned that vec has `windows` and you can create a HashSet with collect 🤯

--- 2022/day06-rust/src/lib.rs
+++ 2022/day06-rust/src/lib.rs
@@ -6,16 +6,16 @@ pub fn read_input(path: &str) -> String {
 }
 
 pub fn get_start(line: &str, win_size: usize) -> usize {
+    // This is mainly for my 'input_small' file because it contains the wanted numbers.
     let parts = line.split_whitespace().collect::<Vec<_>>();
     let code = parts[0];
     let chars = code.chars().collect::<Vec<_>>();
 
     let mut result = 0;
-    for i in 0..code.len() - (win_size - 1) {
-        let window = chars[i..i + win_size].to_vec();
-        let set: HashSet<char> = HashSet::from_iter(window);
+    for (idx, window) in chars.windows(win_size).enumerate() {
+        let set: HashSet<_> = window.iter().collect();
         if set.len() == win_size {
-            result = i + win_size;
+            result = idx + win_size;
             break;
         }
     }
