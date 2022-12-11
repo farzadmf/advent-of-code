037cea1adc06f48aeef7daf0deabe1fd17d75cd2
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2022-12-11 16:36:24 -0500

    22-D11: add template and input files

---
+++ 2022/rust/src/bin/day11_p1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("+++++++++++++++++++ PART 1 +++++++++++++++++++");
+    let result = 0;
+
+    // Let's do this!
+
+    println!("result: {}", result);
+    println!("------------------- PART 1 -------------------");
+}
