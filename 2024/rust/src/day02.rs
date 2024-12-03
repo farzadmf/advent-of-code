05e0b58a6170a04320c394ee5663f84405b1df4d
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-12-02 23:37:15 -0500

    2024: implement day 2 part 1

--- 2024/rust/src/day02.rs
+++ 2024/rust/src/day02.rs
@@ -1,5 +1,35 @@
 pub fn part01(input: &str) -> i32 {
-    input.lines().count().try_into().unwrap()
+    input
+        .lines()
+        .map(|line| {
+            let values: Vec<i32> = line
+                .split_whitespace()
+                .map(|c| c.parse::<i32>().unwrap())
+                .collect();
+
+            let total = values.len();
+            let is_increasing = values[0] < values[total - 1];
+            let mut is_safe = true;
+
+            for i in 0..values.len() - 1 {
+                let (cur, nxt) = (values[i], values[i + 1]);
+
+                if (nxt == cur)
+                    || (is_increasing && nxt < cur)
+                    || (!is_increasing && nxt > cur)
+                    || (nxt - cur).abs() > 3
+                {
+                    is_safe = false;
+                    break;
+                }
+            }
+
+            is_safe
+        })
+        .filter(|value| *value)
+        .count()
+        .try_into()
+        .unwrap()
 }
 
 pub fn part02(input: &str) -> i32 {
@@ -12,8 +42,15 @@ mod tests {
 
     #[test]
     fn test_part01() {
-        let input = "part01";
-        assert_eq!(1, part01(input.trim()));
+        let input = "
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
+        assert_eq!(2, part01(input.trim()));
     }
 
     #[test]
