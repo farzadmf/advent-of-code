7c4dae6713281d045a3956bbab807a15cdcdc9ce
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-12-02 23:36:02 -0500

    2024: add day 2

---
+++ 2024/rust/src/bin/day02_part01.rs
@@ -0,0 +1,10 @@
+use rust::day02::part01;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../input/day02").unwrap();
+
+    println!("+++++++++++++++++++++++ PART 01 ++++++++++++++++++++++++++++");
+    println!("{}", part01(&file));
+    println!("----------------------- PART 01 ----------------------------");
+}
