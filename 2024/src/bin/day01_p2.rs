4e77ec1453e80ca71e4bc53aed95bdb48410efe5
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-12-02 23:31:30 -0500

    2024: remove unused src directory

--- 2024/src/bin/day01_p2.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    println!("+++++++++++++++++++ PART 2 +++++++++++++++++++");
-    let result = 0;
-
-    // Let's do this!
-
-    println!("result: {}", result);
-    println!("------------------- PART 2 -------------------");
-}
