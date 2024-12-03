4c97204aa282f6c19cff10b65d5fd0081c2b9c2d
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-12-03 14:35:06 -0500

    2024-D3: add template

---
+++ 2024/rust/src/day03.rs
@@ -0,0 +1,24 @@
+pub fn part01(input: &str) -> i32 {
+    input.lines().count().try_into().unwrap()
+}
+
+pub fn part02(input: &str) -> i32 {
+    input.lines().count().try_into().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part01() {
+        let input = "part01";
+        assert_eq!(1, part01(input.trim()));
+    }
+
+    #[test]
+    fn test_part02() {
+        let input = "part02";
+        assert_eq!(1, part02(input.trim()));
+    }
+}
