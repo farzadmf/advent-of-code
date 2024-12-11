19aac9df53a2c0007b137aa151e005a43e67dfd4
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2024-12-11 17:14:32 -0500

    2024-D9: well, managed to do part 1!!!

--- 2024/rust/src/day09.rs
+++ 2024/rust/src/day09.rs
@@ -1,61 +1,57 @@
-pub fn part01(input: &str) -> i32 {
+#[derive(Debug, Copy, Clone)]
+struct Value(i64);
+
+pub fn part01(input: &str) -> i64 {
     let mut converted = "".to_string();
     let mut id = 0;
 
-    println!("{:+^60}", " EVENS ");
+    let mut converted2 = vec![];
+
     input.chars().enumerate().for_each(|(pos, c)| {
         let value = c.to_string().parse().unwrap();
 
         let mut to_add = ".".to_string();
+        let mut to_add2 = -1;
         if pos % 2 == 0 {
+            to_add2 = id;
             to_add = id.to_string();
             id += 1;
         }
 
         converted.push_str(to_add.repeat(value).as_str());
+        for _ in 0..value {
+            converted2.push(Value(to_add2));
+        }
     });
 
-    id = 0;
-    let my = input
-        .chars()
-        .enumerate()
-        .fold("".to_string(), |acc, (pos, c)| {
-            let count = c.to_string().parse().unwrap();
-            let mut to_add = ".".to_string();
-
-            if pos % 2 == 0 {
-                to_add = id.to_string();
-                id += 1;
-            }
-
-            format!("{}{}", acc, to_add.repeat(count))
-        });
-
-    let mut my: Vec<_> = my.as_str().chars().collect();
-
+    let mut my = converted2;
     let (mut l, mut r) = (0, my.len() - 1);
     while l < r {
-        while my[l] != '.' {
+        while my[l].0 != -1 {
             l += 1;
         }
-        while my[r] == '.' {
+        while my[r].0 == -1 {
             r -= 1;
         }
 
-        if l < r && my[l] == '.' && my[r] != '.' {
+        if l < r && my[l].0 == -1 && my[r].0 != -1 {
             (my[l], my[r]) = (my[r], my[l]);
             (l, r) = (l + 1, r - 1);
         }
     }
 
     let result = my.iter().enumerate().fold(0, |acc, (pos, c)| {
-        acc + pos * (c.to_string().parse::<usize>().unwrap_or(0))
+        if c.0 == -1 {
+            return acc;
+        }
+
+        acc + (pos as i64) * (c.0)
     });
 
-    result as i32
+    result
 }
 
-pub fn part02(input: &str) -> i32 {
+pub fn part02(input: &str) -> i64 {
     input.lines().count().try_into().unwrap()
 }
 
