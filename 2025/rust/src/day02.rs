15a7ed220105e0c2e5f207f2ff46ccb0579fb45a
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-12-02 23:50:58 -0500

    2025-D2: part2 (Kiro help)
    Signed-off-by: Farzad Majidfayyaz <farzadmf@users.noreply.github.com>

--- 2025/rust/src/day02.rs
+++ 2025/rust/src/day02.rs
@@ -19,8 +19,29 @@ pub fn part01(input: &str) -> i64 {
         .sum::<i64>()
 }
 
-pub fn part02(input: &str) -> i32 {
-    input.lines().count().try_into().unwrap()
+pub fn part02(input: &str) -> i64 {
+    input
+        .split(',')
+        .map(|range| {
+            range
+                .split('-')
+                .map(|values| values.parse::<i64>().expect("no number?"))
+                .collect::<Vec<_>>()
+        })
+        .map(|numbers| numbers[0]..=numbers[1])
+        .flat_map(|range| range.collect::<Vec<_>>())
+        .map(|number| number.to_string())
+        .filter(|number| {
+            let len = number.len();
+            for i in 1..=len / 2 {
+                if len % i == 0 && number[..i].repeat(len / i) == *number {
+                    return true;
+                }
+            }
+            false
+        })
+        .map(|number| number.parse::<i64>().expect("no number?"))
+        .sum::<i64>()
 }
 
 #[cfg(test)]
@@ -35,7 +56,7 @@ mod tests {
 
     #[test]
     fn test_part02() {
-        let input = "part02";
-        assert_eq!(1, part02(input.trim()));
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!(4174379265, part02(input.trim()));
     }
 }
