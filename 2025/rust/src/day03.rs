3d71d0ac6133f328aa00f715b85bc583602e166c
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-12-03 22:57:42 -0500

    2025-D3: part 1 (using itertools)
    Signed-off-by: Farzad Majidfayyaz <farzadmf@users.noreply.github.com>

--- 2025/rust/src/day03.rs
+++ 2025/rust/src/day03.rs
@@ -1,5 +1,23 @@
+use itertools::Itertools;
+
 pub fn part01(input: &str) -> i32 {
-    input.lines().count().try_into().unwrap()
+    input
+        .lines()
+        .map(|bank| {
+            bank.chars()
+                .combinations(2)
+                .unique()
+                .map(|pairs| {
+                    pairs
+                        .iter()
+                        .collect::<String>()
+                        .parse::<i32>()
+                        .expect("no number?")
+                })
+                .max()
+                .expect("why can't do max")
+        })
+        .sum::<i32>()
 }
 
 pub fn part02(input: &str) -> i32 {
@@ -12,8 +30,13 @@ mod tests {
 
     #[test]
     fn test_part01() {
-        let input = "part01";
-        assert_eq!(1, part01(input.trim()));
+        let input = "
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+";
+        assert_eq!(357, part01(input.trim()));
     }
 
     #[test]
