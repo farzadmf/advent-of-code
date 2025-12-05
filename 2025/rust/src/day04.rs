b7642fb7dfdc78f6a719558be955b2e8e0829705
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-12-05 00:13:13 -0500

    2025-D4: part2 in the most unidiomatic way possible!!!
    Signed-off-by: Farzad Majidfayyaz <farzadmf@users.noreply.github.com>

--- 2025/rust/src/day04.rs
+++ 2025/rust/src/day04.rs
@@ -41,7 +41,70 @@ pub fn part01(input: &str) -> i32 {
 }
 
 pub fn part02(input: &str) -> i32 {
-    input.lines().count().try_into().unwrap()
+    let mut grid = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let dirs: [(i32, i32); 8] = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ];
+
+    let mut total = 0;
+
+    let mut marked: Vec<(usize, usize)> = Vec::new();
+    let mut temp = grid.clone();
+
+    loop {
+        grid = temp.clone();
+
+        // println!("GRID");
+        // for row in &grid {
+        //     println!("{}", row.iter().collect::<String>());
+        // }
+        // println!();
+
+        let mut found = false;
+        for (row_idx, row) in grid.iter().enumerate() {
+            for (col_idx, cell) in row.iter().enumerate() {
+                if *cell != '@' {
+                    continue;
+                }
+
+                let mut neighs: Vec<char> = Vec::new();
+
+                for (dr, dc) in dirs {
+                    let (nr, nc) = (row_idx as i32 + dr, col_idx as i32 + dc);
+                    if nr >= 0 && nr < grid.len() as i32 && nc >= 0 && nc < row.len() as i32 {
+                        neighs.push(grid[nr as usize][nc as usize]);
+                    }
+                }
+
+                if neighs.iter().filter(|&c| *c == '@').count() < 4 {
+                    marked.push((row_idx, col_idx));
+                    found = true;
+                    total += 1;
+                }
+            }
+        }
+
+        if !found {
+            break;
+        }
+
+        for (r, c) in marked.drain(..) {
+            temp[r][c] = '.';
+        }
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -67,7 +130,18 @@ mod tests {
 
     #[test]
     fn test_part02() {
-        let input = "part02";
-        assert_eq!(1, part02(input.trim()));
+        let input = "
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+";
+        assert_eq!(43, part02(input.trim()));
     }
 }
