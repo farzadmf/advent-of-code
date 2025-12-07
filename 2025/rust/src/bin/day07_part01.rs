ac75410cd0a4bab198dad4dd365b9d2eb1ea5b59
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-12-07 15:45:58 -0500

    2025-D7: init
    Signed-off-by: Farzad Majidfayyaz <farzadmf@users.noreply.github.com>

---
+++ 2025/rust/src/bin/day07_part01.rs
@@ -0,0 +1,10 @@
+use rust::day07::part01;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../input/day07").unwrap();
+
+    println!("+++++++++++++++++++++++ PART 01 ++++++++++++++++++++++++++++");
+    println!("{}", part01(file.trim()));
+    println!("----------------------- PART 01 ----------------------------");
+}
---
+++ 2025/rust/src/bin/day07_part02.rs
@@ -0,0 +1,10 @@
+use rust::day07::part02;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("../input/day07").unwrap();
+
+    println!("+++++++++++++++++++++++ PART 02 ++++++++++++++++++++++++++++");
+    println!("{}", part02(file.trim()));
+    println!("----------------------- PART 02 ----------------------------");
+}
