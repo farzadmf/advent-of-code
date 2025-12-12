a5c1926c0d93f4cff8dd1390966df63b479ab6c2
Farzad Majidfayyaz <farzadmf@users.noreply.github.com>
2025-12-12 16:16:51 -0500

    2025-D11: init
    Signed-off-by: Farzad Majidfayyaz <farzadmf@users.noreply.github.com>

---
+++ 2025/rust/src/day11.rs
@@ -0,0 +1,27 @@
+pub fn part01(input: &str) -> i32 {
+    input.lines().count().try_into().unwrap()
+}
+
+pub fn part02(input: &str) -> i32 {
+    input.lines().count().try_into().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn input() -> &'static str {
+        "
+"
+    }
+
+    #[test]
+    fn test_part01() {
+        assert_eq!(1, part01(input().trim()));
+    }
+
+    #[test]
+    fn test_part02() {
+        assert_eq!(1, part02(input().trim()));
+    }
+}
